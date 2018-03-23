1fde1fec645d269ab9bf6bf6057757ee2b656c95
jaboyles <jacobboyles0115@gmail.com>
2018-03-23 13:06:08 -0400

    pls work

--- src/main/java/com/revature/hydra/question/QuestionRepositoryServiceApplication.java
+++ src/main/java/com/revature/hydra/question/QuestionRepositoryServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -18,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableSwagger2
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableDiscoveryClient
 @EntityScan(value = "com.revature.beans")
 @EnableAutoConfiguration
