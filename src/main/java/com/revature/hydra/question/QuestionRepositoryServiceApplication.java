b0b12d8ed970e5231bd388bf04d9cfc3dc5ce4c8
jaboyles <jacobboyles0115@gmail.com>
2018-03-23 10:42:14 -0400

    pls work

--- src/main/java/com/revature/hydra/question/QuestionRepositoryServiceApplication.java
+++ src/main/java/com/revature/hydra/question/QuestionRepositoryServiceApplication.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@EnableSwagger2
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableDiscoveryClient
 @EntityScan(value = "com.revature.beans")
 public class QuestionRepositoryServiceApplication {
