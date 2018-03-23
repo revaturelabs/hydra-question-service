21723e28b5038c991fd8a5f454e02e3fea5a5fd4
jaboyles <jacobboyles0115@gmail.com>
2018-03-23 11:37:18 -0400

    pls work

--- src/main/java/com/revature/hydra/question/QuestionRepositoryServiceApplication.java
+++ src/main/java/com/revature/hydra/question/QuestionRepositoryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
